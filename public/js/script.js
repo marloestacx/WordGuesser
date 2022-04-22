0fc4de338a4d68cd106f1bf6cf18e039b3117056
marloestacx <marloes.tacx@live.nl>
2022-04-22 12:12:32 +0200

    Create script

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+let socket = io();
+let messages = document.querySelector("section ul");
+let input = document.querySelector("input");
+let amounts = document.querySelector("p");
+var connectCounter = 0;
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (input.value) {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("message", (message) => {
+  messages.appendChild(
+    Object.assign(document.createElement("li"), { textContent: message })
+  );
+  messages.scrollTop = messages.scrollHeight;
+});
+
+socket.on("userConnected", (connectCounter) => {
+  console.log("test");
+  amounts.appendChild(
+    Object.assign(document.createElement("p"), { textContent: connectCounter })
+  );
+});
